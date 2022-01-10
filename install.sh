4204288e4fb067f5f9c951aa1ce7df20d5451360
Martin LOUAZEL <mart1louazel@gmail.com>
2022-01-10 20:56:24 +0100

    general: replace dependencies file with install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt install \
+	vim \
+	xterm \
+	tmux \
+	xclip \
+	fonts-powerline \
+	zsh
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Zsh - PowerLevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Zsh - Synthax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Zsh - Autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
