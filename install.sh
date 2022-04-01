e5b8cc35c2b1c22bacd9468ac30115c9ce9f4869
Martin Louazel <martin.louazel@streamunlimited.com>
2022-04-01 17:32:34 +0200

    general: add ranger to repo

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ sudo apt install \
 	xclip \
 	fonts-powerline \
 	zsh \
-	fzf
+	fzf \
+	ranger
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
