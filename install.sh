91c82c2cacf36e7667eca41022046fa9584d8da6
Martin Louazel <martin.louazel@streamunlimited.com>
2023-04-21 11:39:49 +0200

    install: fix cloning of p10k

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo apt install \
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Zsh - PowerLevel10k
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Zsh - Synthax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
